142763121f0f049b7eb88c4382cec88209b967b0
Matt Raible <matt.raible@okta.com>
2023-01-25 21:09:24 -0700

    Update to TS errors

---
+++ src/app/search/search.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import { Person, SearchService } from '../shared';
+
+@Component({
+  selector: 'app-search',
+  templateUrl: './search.component.html',
+  styleUrls: ['./search.component.css']
+})
+export class SearchComponent implements OnInit {
+  query: string = '';
+  searchResults: Array<Person> = [];
+
+  constructor(private searchService: SearchService) { }
+
+  ngOnInit(): void { }
+
+  search(): void {
+    this.searchService.search(this.query).subscribe({
+      next: (data: any) => {
+        this.searchResults = data;
+      },
+      error: error => console.log(error)
+    });
+  }
+
+}
